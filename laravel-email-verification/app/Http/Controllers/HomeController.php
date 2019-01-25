09963c26102d34d5eabeab3b3dd2c8348d072b09
sheptianbagjautama <sheptian96@gmail.com>
2019-01-26 02:20:41 +0700

    upload laravel-email-verification modul

---
+++ laravel-email-verification/app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // Menambahkan middleware verified untuk email verification
+        $this->middleware(['auth','verified']);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
---
+++ laravel-email-verification/resources/views/profile.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile</title>
+</head>
+<style>
+body{
+    background-color: darkgreen
+}
+#profile{
+    height: 500px;
+    width: 500px;
+    position: relative;
+    -moz-outline-radius: 20px;
+    -moz-transition: cubic-bezier(0.075, 0.82, 0.165, 1);
+}
+</style>
+<body>
+    <div id="profile">
+        Halo ini adalah halaman profile
+    </div>
+</body>
+</html>
